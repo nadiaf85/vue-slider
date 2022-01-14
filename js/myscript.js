d3e604f4477ef205772fb37172208e647a4c2422
nadiaf85 <nadia.fiorio@alice.it>
2022-01-14 15:54:05 +0100

    push 2

--- js/myscript.js
+++ js/myscript.js
@@ -1,29 +1,38 @@
 let app = new Vue ({
     el : '#miapp',
     data : {
-            items : [
-                    'img/01.jpg',
-                    'img/02.jpg',
-                    'img/03.jpg',
-                    'img/04.jpg',
-                    'img/05.jpg'
-                ],
-            title : [
-                    'Svezia',
-                    'Svizzera',
-                    'Gran Bretagna',
-                    'Germania',
-                    'Paradise'
-                ],
-            text : [
-                    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-                    'Lorem ipsum',
-                    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-                    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-                    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-                ]    
+        corrente:0,
+        items : [
+            'img/01.jpg',
+            'img/02.jpg',
+            'img/03.jpg',
+            'img/04.jpg',
+            'img/05.jpg'
+            ],
+        title : [
+            'Svezia',
+            'Svizzera',
+            'Gran Bretagna',
+            'Germania',
+            'Paradise'
+            ],
+        text : [
+            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            'Lorem ipsum',
+            'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            ]    
         
-            }
+        },
+    // methods: {
+    //     isCorrente:function(indexImmagine){
+    //         if(indexImmagine == this.corrente){
+    //             return "active"
+    //         }
+    //         return "";
+    //     }
+    // }
     });
 
 
